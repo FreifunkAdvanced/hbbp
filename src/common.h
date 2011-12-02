b8ca97e5b02d74c4146808948259a7489fd9b165
miau <miaukatzemiau@priveasy.de>
2011-12-02 10:34:30 +0100

    update max msg len to IPv6 UDP minimum

--- src/common.h
+++ src/common.h
@@ -10,9 +10,14 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
-#define SERVERPORT 4950    // the port users will be connecting to
+/* the port users will be connecting to */
+#define SERVERPORT 4950
 #define SERVERPORT_S "4950"
-#define MAXBUFLEN 100
+
+/* RFC 2460 requires IPv6 link MTU >= 1280, IPv6 UDP headers sum up to
+   48 byte, the rest is for us. One extra byte is for the final \0
+   (not transmitted) */
+#define MAXBUFLEN 1233
 
 #define ENP(Cmd, Msg) \
   if ((Cmd) == -1) {		\
